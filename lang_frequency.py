e907c4d99c74f9b2857108d7313df1a6f1b6f916
Dmitrii Sokolov <sokolovdp@gmail.com>
2017-05-29 19:40:41 +0300

    "1st version"

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8
+
+import sys
+import re
+from collections import Counter
 
 
-def load_data(filepath):
-    pass
+def load_data(filename: "str") -> "list":
+    with open(filename, 'r', encoding='utf-8') as f:
+        raw_text = f.read().lower()
+    return [word for word in re.split('[^a-z]', raw_text) if word]
 
 
-def get_most_frequent_words(text):
-    pass
+def get_most_frequent_words(words: "list") -> "list":
+    text_dict = Counter(words)
+    return [key for key in sorted(text_dict, key=text_dict.get, reverse=True)][:10]
+
+
+def main(filename):
+    words = load_data(filename)
+    print(', '.join(get_most_frequent_words(words)))
 
 
 if __name__ == '__main__':
-    pass
+    main(sys.argv[1:][0])
